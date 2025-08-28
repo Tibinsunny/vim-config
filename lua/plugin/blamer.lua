f08a2ab88335fb1d2daf41b101d0e96c35e4714b
tibinsunny <tibinsunny95@gmail.com>
2025-08-28 13:05:25 +0530

    Fixed Override issue

--- lua/plugin/blamer.lua
+++ lua/plugin/blamer.lua
@@ -1,23 +1,16 @@
 
 return {
-  {
-    "APZelos/blamer.nvim",
-    config = function()
-      -- enable blamer on startup
-      vim.g.blamer_enabled = 1
-
-      -- optional configs
-      vim.g.blamer_delay = 500
-      vim.g.blamer_show_in_insert_modes = 0
-      vim.g.blamer_show_in_visual_modes = 0
-      vim.g.blamer_prefix = ' > '
-      vim.g.blamer_template = '<committer> <committer-mail> '
-      vim.g.blamer_date_format = '%d/%m/%y'
-      vim.g.blamer_relative_time = 1
-
-      -- custom highlight (optional)
-      --
-      vim.cmd [[highlight Blamer guifg=#808080 gui=italic blend=30]]
-    end
-  }
+  "APZelos/blamer.nvim",
+  init = function()
+    -- 👇 set before plugin loads
+    vim.g.blamer_enabled = 1
+    vim.g.blamer_prefix = '  '
+    vim.g.blamer_template = '<author>, <author-time> • <summary> • <commit-short>'
+    vim.g.blamer_date_format = '%d/%m/%y'
+    vim.g.blamer_relative_time = 0
+  end,
+  config = function()
+    -- highlight can be set after load
+    vim.cmd [[highlight Blamer guifg=#666666 gui=italic]]
+  end
 }
