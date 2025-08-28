cd73fee1c6611cdc0393f30bc715c2f13511b0d7
Tibinsunny <tibinsunny95@gmail.com>
2025-08-28 11:52:29 +0530

    aded git blamer

---
+++ lua/plugin/blamer.lua
@@ -0,0 +1,22 @@
+
+return {
+  {
+    "APZelos/blamer.nvim",
+    config = function()
+      -- enable blamer on startup
+      vim.g.blamer_enabled = 1
+
+      -- optional configs
+      vim.g.blamer_delay = 500
+      vim.g.blamer_show_in_insert_modes = 0
+      vim.g.blamer_show_in_visual_modes = 0
+      vim.g.blamer_prefix = ' > '
+      vim.g.blamer_template = '<committer> • <summary>'
+      vim.g.blamer_date_format = '%d/%m/%y'
+      vim.g.blamer_relative_time = 1
+
+      -- custom highlight (optional)
+      vim.cmd [[highlight Blamer guifg=lightgrey]]
+    end
+  }
+}
