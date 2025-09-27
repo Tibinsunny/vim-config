fa001512d83a83a11aaf76a3df3b966d8e8aa505
tibinsunny <tibinsunny95@gmail.com>
2025-09-28 00:34:25 +0530

    Added matchup-UX enhanced

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,36 +1,51 @@
+-- File: e.g., lua/plugins/editing.lua or wherever you group your plugins
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" }, -- lazy-load on file open
+  -- Your existing nvim-treesitter configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "javascript",
+          "typescript",
+          "tsx",
+          "python",
+          "rust",
+          "go",
+          "lua",
+          "bash",
+          "json",
+          "yaml",
+          "html",
+          "css",
+          "markdown",
+          "markdown_inline",
+          "vim",
+          "query",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  -- ADD THIS NEW BLOCK for nvim-matchup
+{
+  'andymass/vim-matchup',
+  event = "BufReadPost",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "javascript",
-        "typescript", -- needed for Node.js & React
-        "tsx",        -- for React (JSX/TSX)
-        "python",
-        "rust",
-        "go",
-        "lua",
-        "bash",
-        "json",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "vim",
-        "query",
-      },
-      highlight = {
-        enable = true, -- enable syntax highlighting
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true, -- better indentation
-      },
-      -- The 'autotag' section has been removed from here.
-      -- It is now handled by the separate 'nvim-ts-autotag' plugin file.
-    })
+ vim.api.nvim_set_hl(0, "MatchParen", { bg = "#590000", fg = "#FFFFFF", bold = true })
+    -- Optional: set matchup options via vim.g
+    vim.g.matchup_enabled = 1
+    vim.g.matchup_surround_enabled = 1
   end,
+},
 }
